462b04570a2bb2da82dfdf442bcaa874aa48e91a
Bartlomiej Pycinski <bpycinski@polsl.pl>
2013-04-30 21:32:29 +0200

    Dodanie klasy LandmarkTransformationToolbox

---
+++ fCal/Toolboxes/LandmarkTransformationToolbox.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "h:\qt-src-4.7.4\src\gui\kernel\qwidget.h"
+
+class LandmarkTransformationToolbox :
+	public QWidget
+{
+public:
+	LandmarkTransformationToolbox(void);
+	virtual ~LandmarkTransformationToolbox(void);
+};
